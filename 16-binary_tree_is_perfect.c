0a7385730e1cd099ef4d86b654c159b24ed94936
Youssef <youssefkaramedu@gmail.com>
2023-08-30 04:14:02 -0700

    Add implementation in the same file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: tree
+ *
+ * Return: size int or 0 if null
+ */
+
+size_t binary_size(const binary_tree_t *tree)
+{
+		if (tree == NULL)
+				return (0);
+		return (binary_size(tree->right) + 1 + binary_size(tree->left));
+}
+
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  *
  * @tree: tree node
@@ -15,6 +30,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	l = binary_tree_size(tree);
+	l = binary_size(tree);
 	return (l & (l + 1)) == 0;
 }
