4873fc35d024e431f398d3754e3fdd3cc526840d
Nawaritta <Nor.benhaddou@gmail.com>
2023-08-31 00:32:36 +0000

    fixing task 110: tree traversal added

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,24 +1,55 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_preorder_next - returns the next node to visit in
+ *                            pre-order traversal
+ * @node: a pointer to the current node
+ * Return: a pointer to the next node to visit,
+ *        or NULL if the traversal is complete
+ */
+binary_tree_t *binary_tree_preorder_next(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->left != NULL)
+		return (node->left);
+
+	if (node->right != NULL)
+		return (node->right);
+
+	while (node->parent != NULL)
+	{
+		if (node == node->parent->left && node->parent->right != NULL)
+			return (node->parent->right);
+		node = node->parent;
+	}
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid BST, and 0 if not or if tree is NULL.
+ */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
+	while (tree != NULL)
 	{
-		if (tree->left->n > tree->n)
+		if (tree->left && tree->left->n > tree->n)
 			return (0);
 
-		binary_tree_is_bst(tree->left);
-	}
-	if (tree->right)
-	{
-		if(tree->right->n < tree->n)
-		return (0);
-		binary_tree_is_bst(tree->right);
-	}
+		if (tree->right && tree->right->n < tree->n)
+			return (0);
 
+		tree = binary_tree_preorder_next(tree);
+	}
 
 	return (1);
 }
